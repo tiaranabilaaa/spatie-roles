d06d9129dadcfea14c93f7363e554ac8cd62024e
tiaranabilaaa <160709402+tiaranabilaaa@users.noreply.github.com>
2024-05-30 15:11:49 +0700

    Update file
    tambah kode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,16 +7,12 @@ use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /** 
-     * Register any application services. 
-     */
+   
     public function register(): void
     {
         // 
     }
-    /** 
-     * Bootstrap any application services. 
-     */
+    
     public function boot(): void
     {
         Paginator::useBootstrapFive();
